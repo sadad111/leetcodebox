8bf4c5290349832a5d5e769db38d229a8e714e77
sadad111 <1501210385@pku.edu.cn>
2017-07-06 14:57:19 +0800

    supplement to java solution

--- Single Element in a Sorted Array.py
+++ Single Element in a Sorted Array.py
@@ -12,3 +12,16 @@ class Solution(object):
             else:
                 hi = mid
         return nums[lo]
+
+# public class Solution {
+#     public int singleNonDuplicate(int[] nums) {
+#                 // binary search
+#         int n=nums.length, lo=0, hi=n/2;
+#         while (lo < hi) {
+#             int m = (lo + hi) / 2;
+#             if (nums[2*m]!=nums[2*m+1]) hi = m;
+#             else lo = m+1;
+#         }
+#         return nums[2*lo];
+#     }
+# }
